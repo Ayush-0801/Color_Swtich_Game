795d80352edd75e072f1d557bde8151f49f173de
Vinay Pandey <55681323+Vinay2305@users.noreply.github.com>
2020-12-03 22:01:42 +0530

    Update UniquePatterns.java

--- src/UniquePatterns.java
+++ src/UniquePatterns.java
@@ -1,94 +1,109 @@
-import javafx.animation.ParallelTransition;
-import javafx.scene.Group;
 import javafx.animation.Interpolator;
 import javafx.animation.PathTransition;
-import javafx.animation.RotateTransition;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
+import javafx.scene.*;
 import javafx.scene.shape.*;
 import javafx.util.Duration;
-import java.util.ArrayList;
-import java.util.Random;
 
-public class UniquePatterns extends Obstacle {
+public class UniquePatterns extends Obstacle{
 
-    private Path tripa;
-    private double h;
+    private double radius=15;
+    private double diameter=radius*2;
+    private Duration rotation=Duration.millis(4500);
 
     public UniquePatterns(double x,double y, double sl)
     {
         super(x,y);
-        h=y;
-        tripa = new Path();
-        MoveTo moveTo = new MoveTo(x, y);
-        tripa.getElements().add(moveTo);
-        LineTo m1 =new LineTo(x+sl,y);
-        LineTo m2 =new LineTo(x+sl/2,y-sl);
-        LineTo m3=new LineTo(x,y);
-        tripa.getElements().addAll(m1,m2,m3);
+        double currentx=x, currenty=y;
+
+        double v1_x = x, v1_y = y;
+        double v2_x = x + sl/(double)2, v2_y = y+sl*Math.sqrt(3)/(double)2;
+        double v3_x = x - sl/(double)2, v3_y = y+sl*Math.sqrt(3)/(double)2;
+
+        g=new Group();
+
+        while(true)
+        {
+            Circle obj = new Circle(currentx, currenty, radius);
+
+            if(g.getChildren().size()<7) {
+                obj.setFill(purple);
+            }
+
+            else if(g.getChildren().size()<14)
+            {
+                obj.setFill(pink);
+            }
+
+            else if(g.getChildren().size()<21)
+            {
+                obj.setFill(blue);
+            }
+
+            else
+            {
+                obj.setFill(yellow);
+            }
+
+            g.getChildren().add(obj);
+
+            if(g.getChildren().size()<=sl/diameter) {
+
+                Polygon pth=new Polygon(currentx, currenty, v2_x, v2_y, v3_x, v3_y,  v1_x, v1_y, currentx, currenty);
+                PathTransition path = new PathTransition();
+                path.setNode(obj);
+                path.setPath(pth);
+                path.setDuration(rotation);
+                path.setCycleCount(-1);
+                path.setInterpolator(Interpolator.LINEAR);
+                path.play();
+
+                currentx += diameter / (double) 2;
+                currenty += diameter * Math.sqrt(3) / (double) 2;
+            }
+
+            else if(g.getChildren().size()<=2*sl/diameter) {
+
+                Polygon pth=new Polygon(currentx, currenty, v3_x, v3_y,  v1_x, v1_y, v2_x, v2_y,currentx, currenty);
+                PathTransition path = new PathTransition();
+                path.setNode(obj);
+                path.setPath(pth);
+                path.setDuration(rotation);
+                path.setCycleCount(-1);
+                path.setInterpolator(Interpolator.LINEAR);
+                path.play();
+
+                currentx -= diameter;
+            }
+
+            else if(g.getChildren().size()<=3*sl/diameter)
+            {
+                Polygon pth=new Polygon(currentx, currenty, v1_x, v1_y, v2_x, v2_y,  v3_x, v3_y, currentx, currenty);
+                PathTransition path = new PathTransition();
+                path.setNode(obj);
+                path.setPath(pth);
+                path.setDuration(rotation);
+                path.setCycleCount(-1);
+                path.setInterpolator(Interpolator.LINEAR);
+                path.play();
+
+                currentx += diameter/2;
+                currenty -= diameter * Math.sqrt(3)/(double)2;
+            }
+
+            if(g.getChildren().size()>=3*sl/diameter)
+            {
+                break;
+            }
+        }
 
         formation();
         this.setObstacleType("DottedObs");
         setIntialBounds();
-
     }
 
     @Override
     public void formation() {
 
-        ArrayList<Circle> circle=new ArrayList<>();
-        for(int i=0;i<24;i++)
-        {
-            Circle smc=new Circle();
-            smc.setCenterX(300);
-            smc.setCenterY(h);
-            smc.setRadius(10);
-
-            if(i>5 && i<12)
-            {
-                smc.setFill(blue);
-            }
-            else if(i>11 && i<18)
-            {
-                smc.setFill(yellow);
-            }
-            else if(i>17 && i<24)
-            {
-                smc.setFill(purple);
-            }
-            else
-            {
-                smc.setFill(pink);
-            }
-            circle.add(smc);
-        }
-        g=new Group();
-        g.getChildren().addAll(circle);
-        transition(207,circle);
-    }
-
-    public void transition(int t1, ArrayList<Circle> cc)
-    {
-        int alpha = Integer.MAX_VALUE;
-        ParallelTransition seqT=new ParallelTransition();
-
-        for(int i=0;i<g.getChildren().size();i++)
-        {
-            PathTransition pathTransition = new PathTransition();
-            pathTransition.setDuration(Duration.millis(5000));
-            pathTransition.setNode(cc.get(i));
-            pathTransition.setDelay(Duration.millis(t1*(i)));
-            pathTransition.setPath(tripa);
-            pathTransition.setCycleCount(alpha);
-            pathTransition.setAutoReverse(false);
-            pathTransition.setInterpolator(Interpolator.LINEAR);
-//            pathTransition.play();
-
-            seqT.getChildren().add(pathTransition);
-
-            //playing the transition
-        }
-        seqT.play();
     }
 
     @Override
